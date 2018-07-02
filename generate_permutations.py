946b799e05a3196fc5c11a4f2db20540993dd913
vitali-shevtsov <vitali.shevtsov@gmail.com>
2018-07-02 16:23:53 +0300

    Generate Permutations using recursion.

---
+++ generate_permutations.py
@@ -0,0 +1,17 @@
+def generate_permutations(N:int, M:int=-1, prefix=None):
+    """Generation all permutations of N numbers in M positions
+       with prefix
+    """
+    M = N if M == -1 else M # by default N numbers in N positions
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for number in range(1, N+1):
+        if number in prefix:
+            continue
+        prefix.append(number)
+        generate_permutations(N, M-1, prefix)
+        prefix.pop()
+
+generate_permutations(9,9)
